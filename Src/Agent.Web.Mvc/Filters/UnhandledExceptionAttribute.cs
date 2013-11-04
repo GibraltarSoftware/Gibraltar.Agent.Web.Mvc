7ba19cf002f376f45e14d4260168483de5ef1cd8
Kendall Miller <kendall@gibraltarsoftware.com>
2013-11-04 18:08:53 -0500

    Updated unhandled exception attribute to pull common configuration

--- Src/Agent.Web.Mvc/Filters/UnhandledExceptionAttribute.cs
+++ Src/Agent.Web.Mvc/Filters/UnhandledExceptionAttribute.cs
@@ -1,6 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net;
+using System.Web;
 using System.Web.Mvc;
+using Gibraltar.Agent.Web.Mvc.Configuration;
+using Gibraltar.Agent.Web.Mvc.Filters.Internal;
+using Gibraltar.Agent.Web.Mvc.Internal;
 
 namespace Gibraltar.Agent.Web.Mvc.Filters
 {
@@ -9,12 +14,34 @@ namespace Gibraltar.Agent.Web.Mvc.Filters
     /// </summary>
     public class UnhandledExceptionAttribute : HandleErrorAttribute 
     {
+        private readonly MvcAgentElement _configuration;
+
+        /// <summary>
+        /// Monitor MVC requests using the site-wide configuration settings
+        /// </summary>
+        public UnhandledExceptionAttribute()
+            : this(MvcAgentElement.SafeLoad())
+        {
+        }
+
+        /// <summary>
+        /// Monitor MVC requests using the provided configuration settings
+        /// </summary>
+        /// <param name="configuration"></param>
+        public UnhandledExceptionAttribute(MvcAgentElement configuration)
+        {
+            if (configuration == null)
+                throw new ArgumentNullException("configuration");
+
+            _configuration = configuration;
+        }
+
         public override void OnException(ExceptionContext filterContext)
         {
             //here we want to interrogate the filter context for route information, 
             //view/controller/model information and create a better message than
             //our default record exception would.
-            Log.RecordException(filterContext.Exception, "Web Site.Exceptions", true);
+            Log.RecordException(filterContext.Exception, MvcAgentElement.LogCategory + ".Exceptions", true);
             base.OnException(filterContext);
         }
     }
