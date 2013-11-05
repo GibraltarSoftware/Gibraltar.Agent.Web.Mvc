e6ee5bfca60476b035131f4d38047122f66a1dd8
Kendall Miller <kendall@gibraltarsoftware.com>
2013-11-04 22:23:04 -0500

    Added web API

---
+++ Src/Agent.Web.Mvc/Filters/Internal/WebApiRequestMetric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace Gibraltar.Agent.Web.Mvc.Filters.Internal
+{
+    /// <summary>
+    /// The tracking metric for a Web API request
+    /// </summary>
+    internal class WebApiRequestMetric : RequestMetric
+    {
+        public WebApiRequestMetric(HttpActionContext actionContext)
+        {
+            var parameters = actionContext.ActionDescriptor.GetParameters().Select(p => p.ParameterName);
+            Parameters = String.Join(", ", parameters);
+
+            HttpMethod = HttpContext.Current.Request.HttpMethod;
+            var controllerDescriptor = actionContext.ActionDescriptor.ControllerDescriptor;
+            ControllerName = controllerDescriptor.ControllerName;
+            ActionName = actionContext.ActionDescriptor.ActionName;
+
+            SubCategory = "Web API";
+
+            //resolve the exact controller target.
+            ClassName = controllerDescriptor.ControllerType.FullName;
+            MethodName = actionContext.ActionDescriptor.ActionName; //BUGBUG: This is wrong; we want the real method
+            FileName = null;
+            LineNumber = 0;
+        }
+    }
+}
