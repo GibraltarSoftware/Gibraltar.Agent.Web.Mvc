255f6b0ac80ed120a0bca56789db014bfc3b0912
Kendall <kendall@gibraltarsoftware.com>
2013-11-07 11:45:51 -0500

    Corrected NuGet pack command to explicitly reference release build profile

--- Src/Agent.Web.Mvc/Filters/Internal/RequestMetric.cs
+++ Src/Agent.Web.Mvc/Filters/Internal/RequestMetric.cs
@@ -17,15 +17,12 @@
 // */
 #endregion
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Web;
 using Gibraltar.Agent.Metrics;
 
 namespace Gibraltar.Agent.Web.Mvc.Filters.Internal
 {
+    [EventMetric("Loupe", "Web Site", "Requests", Caption = "Requests", Description = "Performance data for every call to an MVC controller or Web API controller in the application")]
     internal abstract class RequestMetric: IMessageSourceProvider
     {
         private readonly Stopwatch _timer;
@@ -36,27 +33,32 @@ namespace Gibraltar.Agent.Web.Mvc.Filters.Internal
             _timer = Stopwatch.StartNew();
         }
 
+        [EventMetricValue("startTimestamp", SummaryFunction.Count, null, Caption = "Started", Description = "Timestamp the request started")]
         public DateTimeOffset StartTimestamp { get; set; }
 
         /// <summary>
         /// The duration the request has been running
         /// </summary>
         /// <remarks>Once the request has been told to record it the timer duration will stop increasing.</remarks>
+        [EventMetricValue("duration", SummaryFunction.Average, "ms", Caption = "Duration", Description = "The entire time spent procesing the request, excluding time to return the response", IsDefaultValue = true)]
         public TimeSpan Duration { get { return _timer.Elapsed; }}
 
         /// <summary>
         /// Gets/Sets a String which indicates if the Action was an MVC or WebApi action
         /// </summary>
+        [EventMetricValue("subCategory", SummaryFunction.Count, null, Caption = "Subcategory", Description = "The type of API this request came from (MVC or Web API)")]
         public String SubCategory { get; protected set; }
 
         /// <summary>
         /// Gets/Sets the nme of the controller this action belongs to
         /// </summary>
+        [EventMetricValue("controllerName", SummaryFunction.Count, null, Caption = "Controller", Description = "The short-form name of the controller used for the request (not the .NET class name)")]
         public String ControllerName { get; protected set; }
 
         /// <summary>
         /// Gets/sets the name of this action
         /// </summary>
+        [EventMetricValue("actionName", SummaryFunction.Count, null, Caption = "Action", Description = "The short-form name of the action used for the request (not the .NET method name)")]
         public String ActionName { get; protected set; }
 
         /// <summary>
@@ -66,27 +68,31 @@ namespace Gibraltar.Agent.Web.Mvc.Filters.Internal
         /// In MVC, some actions (typically an EDIT) have both definition for both GET and
         /// POST.  This value helps differentiate between those two calls
         /// </remarks>
+        [EventMetricValue("httpMethod", SummaryFunction.Count, null, Caption = "Method", Description = "The HTTP Method (GET, POST, PUT, DELETE, etc) used for this action")]
         public String HttpMethod { get; protected set; }
 
         /// <summary>
         /// Gets/Sets a String that represents the parameters passed to this action
         /// </summary>
         /// <remarks></remarks>
+        [EventMetricValue("parameters", SummaryFunction.Count, null, Caption = "Parameters", Description = "The list of parameters used for this action")]
         public String Parameters { get; protected set; }
 
         /// <summary>
-        /// The unique Id of this request from the framework
+        /// The unique Id of this controller &amp; action from the framework; MVC only.
         /// </summary>
         public string UniqueId { get; protected set; }
 
         /// <summary>
         /// The user name for the action being performed.
         /// </summary>
+        [EventMetricValue("userName", SummaryFunction.Count, null, Caption = "User", Description = "The user associated with the action being performed")]
         public string UserName { get; set; }
 
         /// <summary>
         /// The exception, if any, thrown at the completion of the routine
         /// </summary>
+        [EventMetricValue("exception", SummaryFunction.Count, null, Caption = "Exception", Description = "The exception, if any, thrown at the completion of the routine")]
         public Exception Exception { get; set; }
 
         /// <summary>
@@ -96,7 +102,6 @@ namespace Gibraltar.Agent.Web.Mvc.Filters.Internal
         {
             _timer.Stop(); 
             EventMetric.Write(this);
-            SampledMetric.Write(this);
         }
 
         public string MethodName { get; protected set; }
