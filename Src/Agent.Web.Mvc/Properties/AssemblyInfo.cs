124e5c0b94d0b51d82a2c051ad79ba20bf401943
Kendall Miller <kendall@gibraltarsoftware.com>
2020-10-19 20:56:00 -0400

    Update version number

--- Src/Agent.Web.Mvc/Properties/AssemblyInfo.cs
+++ Src/Agent.Web.Mvc/Properties/AssemblyInfo.cs
@@ -51,5 +51,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.9.0.0")]
-[assembly: AssemblyFileVersion("4.9.5.0")]
-[assembly: AssemblyInformationalVersion("4.9.5.0")]
+[assembly: AssemblyFileVersion("4.9.6.0")]
+[assembly: AssemblyInformationalVersion("4.9.6.0")]
